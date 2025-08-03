84b7d9f322f889bade4533ec4ae29660960b7aaf
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-08-03 21:02:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
 import os
 
-# Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+# Bot token from @Botfather
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22069945"))
+API_ID = int(os.environ["API_ID"])
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "132b647b2919a6954ddc1d4f378fcb55")
+API_HASH = os.environ["API_HASH"]
 
-# Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "1447424345"))
+# Your Owner / Admin ID for Broadcast
+ADMINS = int(os.environ["ADMINS"])
 
-# Your Mongodb Database Url
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+# Your MongoDB Database URL
+DB_URI = os.environ["DB_URI"]
+DB_NAME = os.environ["DB_NAME"]
 
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+# If you want error messages in your personal messages, set to True; otherwise, False
+ERROR_MESSAGE = bool(os.environ.get("ERROR_MESSAGE"))
